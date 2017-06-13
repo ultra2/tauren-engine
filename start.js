05eb921453b8a072ba423aa50132886618ed365c
Iván Zsolt <ivanzsolt@Ballo-MacBook.local>
2017-06-14 01:46:43 +0200

    a

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Start...1.0.14");
+console.log("Start...1.0.17");
 const Engine_1 = require("./Engine");
 var engine = new Engine_1.default();
 engine.run();
